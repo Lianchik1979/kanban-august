75b0ebbd1a4ff081d3402555b56b1a434ef9c0a6
Lianchik <liana1979.lh@gmail.com>
2020-08-09 13:05:22 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import FormCreateTask from "./FormCreateTask";
+import Board from "./Board";
+
+import { v4 as uuidv4 } from 'uuid';
+
+const initialTasks = [
+    {
+        id: uuidv4(),
+        title: 'First Task',
+        priority: 2,
+        status: 'todo'
+    },
+    {
+        id: uuidv4(),
+        title: 'Second Task',
+        priority: 3,
+        status: 'review'
+    }
+];
+
+const statuses = ['todo', 'progress', 'review', 'done'];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    console.log(initialTasks);
+
+    const [tasks, setTasks] = useState(initialTasks);
+
+    return (
+        <div>
+            <FormCreateTask />
+            {
+                statuses.map(el =>
+                <Board
+                status={el}
+                key={el}
+                tasks={tasks}
+                />)
+            }
+        </div>
+    );
 }
 
 export default App;
+
